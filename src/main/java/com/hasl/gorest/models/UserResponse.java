4051d6b896f5231aa4ed24198e1ad7662fa5f89b
Sergey Dubakin <dubakinsergey@gmail.com>
2026-05-16 13:13:42 +0500

    newBranch_UserRequest_UserResponse_one

---
+++ src/main/java/com/hasl/gorest/models/UserResponse.java
@@ -0,0 +1,12 @@
+package com.hasl.gorest.models;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+}
