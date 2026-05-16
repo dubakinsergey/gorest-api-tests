10688b2b5c89adad5f7ea364404987697bb93e4f
Sergey Dubakin <dubakinsergey@gmail.com>
2026-05-16 13:47:01 +0500

    newBranch_UserRequest_UserResponse_tests

--- src/main/java/com/hasl/gorest/models/UserRequest.java
+++ src/main/java/com/hasl/gorest/models/UserRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserRequest {
+
     private String name;
     private String email;
     private String gender;
--- src/main/java/com/hasl/gorest/models/UserResponse.java
+++ src/main/java/com/hasl/gorest/models/UserResponse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserResponse {
+
     private int id;
     private String name;
     private String email;
